71392be43b4d738de63d812a60f1c63e8b621c7a
Jon Connell <python@figsandfudge.com>
2023-08-10 13:26:08 +0100

    File moved

--- src/plugin.py
+++
@@ -1,7 +0,0 @@
-import pytest
-import importlib.metadata
-
-
-@pytest.hookimpl()
-def pytest_addoption(parser) -> None:
-    group = parser.getgroup("maxcov", help=importlib.metadata.version("pytest-maxcov"))
