df9a69612ce4de74f6d89c55c22fa63f7bc1e3b4
Jon Connell <python@figsandfudge.com>
2023-09-24 15:18:02 +0100

    Initial commit

---
+++ tests/test_maxcov.py
@@ -0,0 +1,20 @@
+import pytest
+import os
+
+
+@pytest.mark.script_launch_mode("subprocess")
+def test_two_pass(script_runner, tmpdir_test_env):
+    ret = script_runner.run(
+        ["python3", "-m", "pytest", "--maxcov-record"],
+        print_result=False,
+    )
+    assert ret.stderr == ""
+    assert ret.success
+    assert os.path.exists(".maxcov")
+
+    ret = script_runner.run(
+        ["python3", "-m", "pytest", "--maxcov", "-vvv"],
+        print_result=False,
+    )
+    assert ret.stderr == ""
+    assert ret.success
